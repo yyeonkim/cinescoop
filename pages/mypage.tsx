02fda19b48a04f1d044e926fdf019fca33a3645e
march0320 <march02198@gmail.com>
2022-01-20 04:34:25 +0900

    ✨ Feat: create mypage

---
+++ pages/mypage.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Account from "../src/components/Account";
+
+const MyPage: NextPage = () => {
+  return (
+    <div>
+      <Account />
+    </div>
+  );
+};
+
+export default MyPage;
