a72de7b4ea6cc2cb50214b022e36e73f457b7227
spig0126 <spig0126@gmail.com>
2022-01-20 07:07:06 +0900

    ✨ Feat: Create GridList with VerCard component

---
+++ src/components/GridList.tsx
@@ -0,0 +1,30 @@
+import { Grid } from "@chakra-ui/react";
+
+import VerCard from "./VerCard";
+import type { Movie } from "./VerCard";
+
+interface MovieListProps {
+  data: Movie[];
+  width: string;
+  columnNum: number;
+  rowNum: number;
+}
+
+function GridList({ data, width, columnNum, rowNum }: MovieListProps) {
+  return (
+    <Grid
+      templateColumns={`repeat(${columnNum}, 1fr)`}
+      templateRows={`repeat(${rowNum}, 1fr)`}
+      gap={10}
+      marginX={"auto"}
+      w={width}
+    >
+      {data.map((item: Movie, index: number) => (
+        <VerCard key={item.id} info={item} />
+      ))}
+    </Grid>
+  );
+}
+
+export default GridList;
+export type { Movie };
