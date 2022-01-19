c9bf1088e8400dd4c8cd3f7e350a4f32cf2def8a
march0320 <march02198@gmail.com>
2022-01-20 01:17:26 +0900

    🚚 Rename: move page folder

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Footer from "../src/components/Footer";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
