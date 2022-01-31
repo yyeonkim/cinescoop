0e1cb4ed4e71fe0998f01c15b70b4cd56809cd6a
spig0126 <spig0126@gmail.com>
2022-02-01 00:39:58 +0900

    ✨ Feat: Create MainImageSection

---
+++ src/components/MovieInfo/MainImageSection.tsx
@@ -0,0 +1,32 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+interface MainImageSectionProps {
+  filePath: String;
+}
+
+function MainImageSection({ filePath }: MainImageSectionProps) {
+  return (
+    <Flex marginBottom="1rem">
+      <Image
+        src={`https://www.themoviedb.org/t/p/w1280/${filePath} `}
+        w="25vw"
+        filter="blur(3px)"
+        objectFit="none"
+        objectPosition="left 50%"
+      />
+      <Image
+        src={`https://www.themoviedb.org/t/p/w1280/${filePath} `}
+        w="50vw"
+      />
+      <Image
+        src={`https://www.themoviedb.org/t/p/w1280/${filePath} `}
+        w="25vw"
+        filter="blur(3px)"
+        objectFit="none"
+        objectPosition="right 50%"
+      />
+    </Flex>
+  );
+}
+
+export default MainImageSection;
