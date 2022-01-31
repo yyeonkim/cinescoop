8e5126d33a8b6eb3e3e2dd2b44b97445a82c368b
spig0126 <spig0126@gmail.com>
2022-01-31 16:56:40 +0900

    ✨ Feat: add toggle color mode button

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Button, Flex, Spacer, useColorMode } from "@chakra-ui/react";
 
 import AccountBox from "./Profile";
 import Search from "./Search";
@@ -13,6 +13,8 @@ export interface AccountBoxProps {
 }
 
 function Navigation() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Flex gap={30} paddingX={100} paddingY={10} alignItems="center">
       <p>Logo</p>
@@ -20,6 +22,15 @@ function Navigation() {
       <PageLink path="/about" name="About" />
       <Spacer />
       <Search version="short" />
+      <Button
+        onClick={toggleColorMode}
+        variant="outline"
+        size="md"
+        _focus={{ outline: "none" }}
+        _hover={{ borderColor: "pink" }}
+      >
+        {colorMode == "dark" ? "Dark" : "Light"} Mode
+      </Button>
       <AccountBox login={true} name="Alice Jung" />
     </Flex>
   );
