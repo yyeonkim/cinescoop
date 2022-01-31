f99a0cac07bd2aeedfd3340c3d7415eac0fde530
spig0126 <spig0126@gmail.com>
2022-02-01 00:30:05 +0900

    ✨ Feat: create movieInfoPage

---
+++ pages/movieInfoPage.tsx
@@ -0,0 +1,40 @@
+import { Flex, Image } from "@chakra-ui/react";
+import { AspectRatio } from "@chakra-ui/layout";
+
+import useFetchMovieDetails from "./api/useFetchMovieDetails";
+import useFetchMovieImages from "./api/useFetchMovieImages";
+import {
+  VideosTest,
+  MovieDetails,
+  MovieImages,
+  MovieCredits,
+} from "../src/TestCase";
+import Navigation from "../src/components/Navigation/Navigation";
+import MainImageSection from "../src/components/MovieInfo/MainImageSection";
+import DetailsTicketBox from "../src/components/MovieInfo/DetailsTicketBox";
+import RelatedVideos from "../src/components/MovieInfo/RelatedVides";
+
+function MovieInfoPage() {
+  const { details, images, videos, isLoading, isError } =
+    useFetchMovieDetails();
+
+  return (
+    <>
+      <Navigation />
+      {isLoading ? (
+        <div>loading</div>
+      ) : (
+        <Flex flexDirection="column" alignItems="center" w="100%">
+          <MainImageSection filePath={MovieDetails.backdrop_path} />
+          <DetailsTicketBox details={MovieDetails} credits={MovieCredits} />
+          <RelatedVideos videos={VideosTest.results} />
+          <Flex bgColor="brightBlue" w="80%" minW="10rem">
+            Hello
+          </Flex>
+        </Flex>
+      )}
+    </>
+  );
+}
+
+export default MovieInfoPage;
