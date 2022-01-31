d17271a5a8f9067f684cc9afb737c1b16545c67f
spig0126 <spig0126@gmail.com>
2022-02-01 00:36:29 +0900

    ✨ Feat: Add ICast, ICrew, IMovieDetails, IMovieCredits

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -37,3 +37,66 @@ export interface IGenre {
   id: string;
   name: string;
 }
+
+export interface ICast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface ICrew {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface IMovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: object;
+  budget: number;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object[];
+  production_countries: object[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: object[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IMovieCredits {
+  id: number;
+  cast: ICast[];
+  crew: ICrew[];
+}
