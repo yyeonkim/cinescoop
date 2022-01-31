a83c1740f5316ffb10548ddbb11218e4df18ca38
spig0126 <spig0126@gmail.com>
2022-01-31 16:49:05 +0900

    🥅 Fix: eliminate syntax error

--- src/components/Navigation/Search.tsx
+++ src/components/Navigation/Search.tsx
@@ -8,29 +8,27 @@ function Search({ version }: SearchProps) {
   const [visible, setVisible] = useState(false);
   const handleToggle = () => setVisible(!visible);
 
-  if (version === "short") {
-    return (
-      <Flex w="40%" justifyContent="right">
-        <SlideFade in={visible} offsetX={5} unmountOnExit={true} reverse={true}>
-          <Input
-            variant="flushed"
-            placeholder="영화 제목, 감독 등을 입력해주세요"
-            focusBorderColor="pink"
-            w="30vw"
-          />
-        </SlideFade>
-        <IconButton
-          aria-label="Search database"
-          icon={<SearchIcon />}
-          onClick={handleToggle}
-          background="none"
-          _focus={{ outline: "none" }}
-          _hover={{ backgroundColor: "pink" }}
-          borderRadius="10rem"
+  return (
+    <Flex w="40%" justifyContent="right">
+      <SlideFade in={visible} offsetX={5} unmountOnExit={true} reverse={true}>
+        <Input
+          variant="flushed"
+          placeholder="영화 제목, 감독 등을 입력해주세요"
+          focusBorderColor="pink"
+          w="30vw"
         />
-      </Flex>
-    );
-  }
+      </SlideFade>
+      <IconButton
+        aria-label="Search database"
+        icon={<SearchIcon />}
+        onClick={handleToggle}
+        background="none"
+        _focus={{ outline: "none" }}
+        _hover={{ backgroundColor: "pink" }}
+        borderRadius="10rem"
+      />
+    </Flex>
+  );
 }
 
 export default Search;
