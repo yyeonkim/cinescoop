f5b508e7ba98fda12c5dd0846fbc1abc0e5af2f9
march0320 <march02198@gmail.com>
2022-01-31 23:56:24 +0900

    🐛 fix: search api

--- pages/searchresult.tsx
+++ pages/searchresult.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from "next";
 import useSearchMovie from "./API/useSearchMovie";
 import SearchBar from "../src/components/SearchBar";
 import GridList from "../src/components/GridList";
-import WithoutSearch from "../src/components/Navigation/WithoutSearch";
 import { Flex, Text, Button } from "@chakra-ui/react";
 
 const SearchResultPage: NextPage = () => {
   const { movieData, handleChange } = useSearchMovie();
   return (
     <div>
-      <WithoutSearch />
       <Flex align="center" justifyContent="center" direction="column">
         <Flex mb="3rem">
           <SearchBar handleChange={handleChange} />
