2a0356230c03e2bd6544a5849a448a2d5b8aad03
march0320 <march02198@gmail.com>
2022-02-02 20:08:09 +0900

    🐛 Fix: .env

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,10 +26,9 @@ const Home: NextPage<IHomeProps> = ({
 }) => {
   return (
     <>
-      <Navigation />
+      <Navigation num={1} />
       <PageList data={trending} />
       <HomeText />
-
       <Box bgColor="brightBlue" p={10} py={20}>
         <Flex>
           <Heading size="lg" mb={10} mr={8}>
@@ -61,28 +60,28 @@ export async function getStaticProps() {
   // Trending movies
   const { results: trending } = await (
     await fetch(
-      `${BASE_URL}/trending/movie/day?api_key=${process.env.API_KEY}&language=ko&include_adult=true`
+      `${BASE_URL}/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ko&include_adult=true`
     )
   ).json();
 
   // Now playing movies
   const { results: nowPlaying } = await (
     await fetch(
-      `${BASE_URL}/movie/now_playing?api_key=${process.env.API_KEY}&language=ko&include_adult=true&page=1`
+      `${BASE_URL}/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ko&include_adult=true&page=1`
     )
   ).json();
 
   // Top rated movies
   const { results: topRated } = await (
     await fetch(
-      `${BASE_URL}/movie/top_rated?api_key=${process.env.API_KEY}&language=ko&include_adult=true&page=1`
+      `${BASE_URL}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ko&include_adult=true&page=1`
     )
   ).json();
 
   // Genre ids
   const { genres } = await (
     await fetch(
-      `${BASE_URL}/genre/movie/list?api_key=${process.env.API_KEY}&language=ko`
+      `${BASE_URL}/genre/movie/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ko`
     )
   ).json();
 
