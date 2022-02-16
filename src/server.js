38ab99aeb1d0c89f6aefdeff62bed28155a3d15e
yyeonkim <seyeon.dev@gmail.com>
2022-02-16 10:09:43 +0900

    ✨ Feat: Add server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/reserve/:id[0-9]", (req, res) => {
+    console.log("can you see me?");
+  });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
