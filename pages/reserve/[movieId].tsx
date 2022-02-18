e0b0ce1d27dcc762ce94dfa28952426f7b69bc84
yyeonkim <seyeon.dev@gmail.com>
2022-02-18 14:59:03 +0900

    🧪 Test: Add server side props for crawling

--- pages/reserve/[movieId].tsx
+++ pages/reserve/[movieId].tsx
@@ -3,12 +3,12 @@ import { useQuery } from "react-query";
 import { useRecoilValue } from "recoil";
 import { Flex } from "@chakra-ui/react";
 
-import Navigation from "../../src/components/Navigation/Navigation";
 import { fetchCredit, fetchDetail } from "../api/useFetchGenre";
 import { ICast, IMovieDetails } from "../../src/interfaces";
+import { movieIDState } from "../../src/atom";
+import Navigation from "../../src/components/Navigation/Navigation";
 import MovieDetail from "../../src/components/MovieDetail";
 import ShowTime from "../../src/components/ShowTime";
-import { movieIDState } from "../../src/atom";
 
 const Reserve: NextPage = () => {
   const movieID = useRecoilValue(movieIDState);
@@ -39,3 +39,22 @@ const Reserve: NextPage = () => {
 };
 
 export default Reserve;
+
+export async function getServerSideProps() {
+  const puppeteer = require("puppeteer");
+  const browser = await puppeteer.launch(); // headless 브라우저 실행
+  const page = await browser.newPage(); // 새로운 페이지 열기
+
+  await page.goto(
+    "http://ticket.cgv.co.kr/Reservation/Reservation.aspx?MOVIE_CD=&MOVIE_CD_GROUP=&PLAY_YMD=&THEATER_CD=&PLAY_NUM=&PLAY_START_TM=&AREA_CD=&SCREEN_CD=&THIRD_ITEM="
+  );
+  await page.waitForTimeout(3000);
+  const lists = await page.$$eval("#movie_list > ul > li", (lists) =>
+    lists.map((list) => list)
+  );
+  console.log(lists);
+
+  return {
+    props: {},
+  };
+}
