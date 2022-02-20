ac219cfd89c81f0aaa0a18e6431269a95fce96a1
spig0126 <spig0126@gmail.com>
2022-02-20 15:51:37 +0900

    💥 Feat: change menu items depending on login state, remove id text&drawer icon

--- src/components/Navigation/Profile.tsx
+++ src/components/Navigation/Profile.tsx
@@ -1,4 +1,3 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import {
   Avatar,
@@ -10,33 +9,48 @@ import {
   MenuList,
 } from "@chakra-ui/react";
 import { AiOutlineUser } from "react-icons/ai";
+
 import { AccountBoxProps } from "./Navigation";
 
 function Profile({ login, name }: AccountBoxProps) {
+  const initials = login ? name : "";
+
   return (
     <Flex>
-      <Avatar
-        name="Segun Adebayo"
-        src="https://bit.ly/broken-link"
-        icon={<AiOutlineUser fontSize="1.5rem" />}
-      />
       <Menu>
         <MenuButton
           as={Button}
-          rightIcon={<ChevronDownIcon />}
-          _hover={{ color: "pink" }}
+          boxSize="fit-content"
+          _hover={{
+            background: "none",
+            "&>span>span": {
+              backgroundColor: "pink",
+              transitionDuration: "0.4s",
+            },
+          }}
           _focus={{ outline: "none" }}
           _active={{ background: "none" }}
           background="none"
           alignSelf="center"
+          padding={0}
         >
-          {name}
+          <Avatar
+            name={initials}
+            src="https://bit.ly/broken-link"
+            icon={<AiOutlineUser fontSize="1.5rem" />}
+          />
         </MenuButton>
-        <MenuList>
+        <MenuList position="relative" zIndex={2}>
           <Link href={`/mypage`}>
             <MenuItem>마이페이지</MenuItem>
           </Link>
-          <MenuItem>로그아웃하기</MenuItem>
+          {login ? (
+            <MenuItem>로그아웃하기</MenuItem>
+          ) : (
+            <Link href={`/login`}>
+              <MenuItem>로그인하기</MenuItem>
+            </Link>
+          )}
         </MenuList>
       </Menu>
     </Flex>
