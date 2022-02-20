e8b0bd0ef8b8f1038089f2878b5339bee8993c75
spig0126 <spig0126@gmail.com>
2022-02-20 17:00:22 +0900

    🔧 Config: add firebase config file

---
+++ firebase.tsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
