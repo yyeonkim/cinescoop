17e3a0842b0a9736bd06c64606b0cc3f0f17acfc
yyeonkim <seyeon.dev@gmail.com>
2022-02-21 09:46:07 +0900

    🔥 Remove unused code

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 const express = require("express");
 const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3001;
+const port = parseInt(process.env.PORT, 10) || 3002;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
-
-  server.get("/reserve/:id[0-9]", (req, res) => {
-    res.sendFile(__dirname + "/pages/reserve/[movieId].tsx");
-  });
 
   server.all("*", (req, res) => {
     return handle(req, res);
