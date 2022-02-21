e626a72e149a76f63adc09c3e0a215c477c548ed
yyeonkim <seyeon.dev@gmail.com>
2022-02-21 09:50:33 +0900

    Get areas playing movies

--- pages/reserve/[movieId].tsx
+++ pages/reserve/[movieId].tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { NextPage } from "next";
 import { useQuery } from "react-query";
 import { Flex } from "@chakra-ui/react";
@@ -10,7 +11,6 @@ import ShowTime from "../../src/components/ShowTime";
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 import { movieIDState } from "../../src/atom";
-import { useEffect } from "react";
 
 const Reserve: NextPage = () => {
   const {
@@ -22,7 +22,7 @@ const Reserve: NextPage = () => {
   useEffect(() => {
     const id = parseInt(movieId, 10);
     setMovieId(id);
-  });
+  }, []);
 
   const { data: detailData, isLoading: detailLoading } =
     useQuery<IMovieDetails>(["detail", movieId], () => fetchDetail(movieId));
@@ -50,18 +50,45 @@ const Reserve: NextPage = () => {
 
 export default Reserve;
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ query }: any) {
+  const { movieId } = query;
+  const { title } = await fetchDetail(movieId); // 예매 페이지의 영화 제목
   const puppeteer = require("puppeteer");
-  const browser = await puppeteer.launch(); // headless 브라우저 실행
+  const browser = await puppeteer.launch();
   const page = await browser.newPage(); // 새로운 페이지 열기
 
-  await page.goto(
-    "http://ticket.cgv.co.kr/Reservation/Reservation.aspx?MOVIE_CD=&MOVIE_CD_GROUP=&PLAY_YMD=&THEATER_CD=&PLAY_NUM=&PLAY_START_TM=&AREA_CD=&SCREEN_CD=&THIRD_ITEM="
+  await page.setDefaultNavigationTimeout(0);
+  // 네이버 영화: 현재 상영 영화 페이지로 이동s
+  await page.goto("https://movie.naver.com/movie/running/current.naver");
+
+  // 네이버 영화: 현재 상영작 제목 가져오기
+  const titles = await page.$$eval(
+    "#content > div.article > div > div.lst_wrap > ul > li > dl > dt > a",
+    (lists: any) => lists.map((list: any) => list.textContent)
   );
-  await page.waitForTimeout(3000);
-  const lists = await page.$$eval("#movie_list > ul > li", (lists) =>
-    lists.map((list) => list)
+  const index = titles.findIndex((element: any) => element === title);
+
+  // 네이버 영화: 예매하기 버튼 클릭
+  await page.click(
+    `#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(${
+      index + 1
+    }) > dl > dd.info_t1 > div`
   );
+  // const link = await page.$eval(
+  //   `#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(${
+  //     index + 1
+  //   }) > dl > dd.info_t1 > div > a`,
+  //   (a: any) => a.href
+  // );
+  // // 네이버 영화의 예매 페이지로 이동
+  // await page.goto(link, { waitUntil: "networkidle0", timeout: 0 });
+
+  const areas = await page.$$eval("#rootDropBox > li > a", (lists: any) =>
+    lists.map((list: any) => list.textContent)
+  );
+  console.log(areas);
+
+  await browser.close();
 
   return {
     props: {},
