354c808707296e05ad2006f242c14e312fc27f83
spig0126 <spig0126@gmail.com>
2022-02-28 20:25:59 +0900

    add Navigation cmp

--- pages/join.tsx
+++ pages/join.tsx
@@ -21,6 +21,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
 import { loginState, userState } from "../src/atom";
+import Navigation from "../src/components/Navigation/Navigation";
 
 interface IForm {
   id: string;
@@ -63,22 +64,24 @@ const Join: NextPage = () => {
   const list = ["a", "b", "c"];
 
   return (
-    <Flex h="100vh" direction="column" justify="center" alignItems="center">
-      <Heading size="2xl" mb="1.5rem">
-        회원가입
-      </Heading>
+    <>
+      <Navigation search={true} />
+      <Flex h="100vh" direction="column" justify="center" alignItems="center">
+        <Heading size="2xl" mb="1.5rem">
+          회원가입
+        </Heading>
 
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <FormControl>
-          <Text mt="1.2rem">이메일 *</Text>
-          <Input
-            {...register("email", { required: "이메일을 입력하세요" })}
-            type="email"
-          />
-          <Text fontSize="xs" color="tomato">
-            {errors?.email?.message}
-          </Text>
-          {/* <Text mt="1.2rem">아이디 *</Text>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <Text mt="1.2rem">이메일 *</Text>
+            <Input
+              {...register("email", { required: "이메일을 입력하세요" })}
+              type="email"
+            />
+            <Text fontSize="xs" color="tomato">
+              {errors?.email?.message}
+            </Text>
+            {/* <Text mt="1.2rem">아이디 *</Text>
           <Input
             {...register("id", { required: "아이디를 입력하세요" })}
             type="text"
@@ -87,73 +90,74 @@ const Join: NextPage = () => {
             {errors?.id?.message}
           </Text> */}
 
-          <Text mt="1.2rem">비밀번호 *</Text>
-          <InputGroup size="md">
-            <Input
-              {...register("password", {
-                required: "비밀번호를 입력하세요",
-                minLength: {
-                  value: 8,
-                  message: "8자 이상 입력하세요",
-                },
-              })}
-              type={show ? "text" : "password"}
-            />
-            <InputRightElement width="3rem">
-              {show ? (
-                <Icon boxSize="1.5rem" as={AiFillEye} onClick={clickShow} />
-              ) : (
-                <Icon
-                  boxSize="1.5rem"
-                  as={AiFillEyeInvisible}
-                  onClick={clickShow}
-                />
-              )}
-            </InputRightElement>
-          </InputGroup>
-          <Text fontSize="xs" color="tomato">
-            {errors?.password?.message}
-          </Text>
-          <Text mt="1.2rem">비밀번호 확인 *</Text>
-          <InputGroup size="md">
-            <Input
-              {...register("confirmation", {
-                required: "비밀번호 확인을 입력하세요",
-              })}
-              type={show ? "text" : "password"}
-            />
-            <InputRightElement width="3rem">
-              {show ? (
-                <Icon boxSize="1.5rem" as={AiFillEye} onClick={clickShow} />
-              ) : (
-                <Icon
-                  boxSize="1.5rem"
-                  as={AiFillEyeInvisible}
-                  onClick={clickShow}
-                />
-              )}
-            </InputRightElement>
-          </InputGroup>
-          <Text fontSize="xs" color="tomato">
-            {errors?.confirmation?.message}
-            {watch("password") !== watch("confirmation") &&
-              watch("confirmation").length !== 0 &&
-              "비밀번호가 다릅니다"}
-          </Text>
-        </FormControl>
+            <Text mt="1.2rem">비밀번호 *</Text>
+            <InputGroup size="md">
+              <Input
+                {...register("password", {
+                  required: "비밀번호를 입력하세요",
+                  minLength: {
+                    value: 8,
+                    message: "8자 이상 입력하세요",
+                  },
+                })}
+                type={show ? "text" : "password"}
+              />
+              <InputRightElement width="3rem">
+                {show ? (
+                  <Icon boxSize="1.5rem" as={AiFillEye} onClick={clickShow} />
+                ) : (
+                  <Icon
+                    boxSize="1.5rem"
+                    as={AiFillEyeInvisible}
+                    onClick={clickShow}
+                  />
+                )}
+              </InputRightElement>
+            </InputGroup>
+            <Text fontSize="xs" color="tomato">
+              {errors?.password?.message}
+            </Text>
+            <Text mt="1.2rem">비밀번호 확인 *</Text>
+            <InputGroup size="md">
+              <Input
+                {...register("confirmation", {
+                  required: "비밀번호 확인을 입력하세요",
+                })}
+                type={show ? "text" : "password"}
+              />
+              <InputRightElement width="3rem">
+                {show ? (
+                  <Icon boxSize="1.5rem" as={AiFillEye} onClick={clickShow} />
+                ) : (
+                  <Icon
+                    boxSize="1.5rem"
+                    as={AiFillEyeInvisible}
+                    onClick={clickShow}
+                  />
+                )}
+              </InputRightElement>
+            </InputGroup>
+            <Text fontSize="xs" color="tomato">
+              {errors?.confirmation?.message}
+              {watch("password") !== watch("confirmation") &&
+                watch("confirmation").length !== 0 &&
+                "비밀번호가 다릅니다"}
+            </Text>
+          </FormControl>
 
-        <Button
-          type="submit"
-          mt="3rem"
-          bg="pink"
-          color="darkBlue"
-          py="1rem"
-          w="100%"
-        >
-          가입하기
-        </Button>
-      </StyledForm>
-    </Flex>
+          <Button
+            type="submit"
+            mt="3rem"
+            bg="pink"
+            color="darkBlue"
+            py="1rem"
+            w="100%"
+          >
+            가입하기
+          </Button>
+        </StyledForm>
+      </Flex>
+    </>
   );
 };
 
