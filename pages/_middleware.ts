2037b8b2a9862a1df70d5aceacaa20ee5fc61298
Yongyeon Kim <yeonlotus@ewhain.net>
2022-03-02 18:06:56 +0900

    ✨ Feat: Add middleware to protect mypage
    Only logged-in user can access to mypage

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,12 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+const isLoggedIn = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    return true;
+  }
+  return false;
+});
 
 export function middleware(req: NextRequest) {
+  const { page, nextUrl } = req;
+
   // 로그인 유저만 mypage에 접근 가능
-  //   if (req.url === "http://localhost:3000/mypage" && 로그아웃) {
-  //     Window.alert("로그인이 필요한 서비스입니다")
-  //     const url = req.nextUrl.clone();
-  //     url.pathname = "/login";
-  //     return NextResponse.redirect(url);
-  //   }
+  if (!isLoggedIn && page.name === "/mypage") {
+    const url = nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 }
