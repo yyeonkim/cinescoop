0484b3a8cd11f11e72fa38a9b028ff7caa192e04
spig0126 <spig0126@gmail.com>
2022-03-07 20:48:01 +0900

    🎨 Refactor: structure code with ReuathenticationForm&NewPasswordForm

---
+++ src/components/Account/ChangePasswordModal.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import { Flex, Text } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../atom";
+import ReauthenticateForm from "./ReauthenticateForm";
+import NewPasswordForm from "./NewPasswordForm";
+
+function ChangePasswordModal({ isOpen, onClose }: any) {
+  const [verified, setVerified] = useState(false);
+  const user = useRecoilValue(userState);
+  return (
+    <Modal
+      size="xl"
+      isOpen={isOpen}
+      onClose={() => {
+        onClose();
+        setVerified(false);
+      }}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>비밀번호 변경하기</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Flex justify="center" flexDir="column">
+            {user.thirdParty ? (
+              <Text mb="3rem">
+                외부 소셜 계정으로 로그인하셨습니다. 비밀번호 변경을 원하시면
+                특정 소셜 계정 사이트에서 변경해주시길 바랍니다.
+              </Text>
+            ) : (
+              <>
+                <ReauthenticateForm
+                  verified={verified}
+                  setVerified={setVerified}
+                />
+                <NewPasswordForm
+                  verified={verified}
+                  setVerified={setVerified}
+                  onClose={onClose}
+                />
+              </>
+            )}
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+export default ChangePasswordModal;
