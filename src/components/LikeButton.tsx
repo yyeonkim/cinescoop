7b20eacd45a78420f587effeb2bcebc9c7985c5a
Yongyeon Kim <yeonlotus@ewhain.net>
2022-03-11 14:00:40 +0900

    ✨ Feat: Update DB when logged-in user clicks like button

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -3,17 +3,48 @@ import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { doc, setDoc } from "firebase/firestore";
 
-import { likedState, movieIDState } from "../atom";
+import {
+  likedMoviesState,
+  likedState,
+  loginState,
+  movieIDState,
+  uidState,
+  usernameState,
+} from "../atom";
+import { db } from "../../firebase";
 
 export default function LikeButton() {
   const [liked, setLiked] = useRecoilState(likedState);
+  const isLoggedIn = useRecoilValue(loginState);
   const movieID = useRecoilValue(movieIDState);
+  const userId = useRecoilValue(uidState);
+  const username = useRecoilValue(usernameState);
+  const likedMovies = useRecoilValue(likedMoviesState);
 
-  const onClick = () => {
-    setLiked((current) => !current);
-    // 찜하기 취소
-    if (liked) {
-      console.log(movieID);
+  const onClick = async () => {
+    if (!isLoggedIn) {
+      window.alert("로그인이 필요합니다.");
+    } else {
+      // 버튼 애니메이션
+      setLiked((current) => !current);
+
+      // 찜하기 취소
+      if (liked) {
+        let movies = likedMovies?.filter((movie) => movie.id !== movieID);
+        await setDoc(doc(db, "users", userId), {
+          id: userId,
+          username,
+          movies,
+        });
+      } else {
+        // 찜하기
+        let movies = likedMovies?.push({ id: movieID });
+        await setDoc(doc(db, "users", userId), {
+          id: userId,
+          username,
+          movies,
+        });
+      }
     }
   };
 
