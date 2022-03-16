5b41abd39cda1b10e11c43be92199f1f5a1c36a9
Yongyeon Kim <yeonlotus@ewhain.net>
2022-03-16 12:39:38 +0900

    ✨ Feat: Add logout message when user logs out

--- src/components/Navigation/Profile.tsx
+++ src/components/Navigation/Profile.tsx
@@ -7,21 +7,31 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  useToast,
 } from "@chakra-ui/react";
 import { AiOutlineUser } from "react-icons/ai";
 import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
+
 import { auth } from "../../../firebase";
 
 function Profile() {
   const user = auth.currentUser;
   const router = useRouter();
+  const toast = useToast();
 
   const logout = () => {
     signOut(auth)
       .then(() => {
         console.log("User logged out");
         router.push("/");
+        toast({
+          title: "로그아웃 완료",
+          description: "로그아웃 되었습니다.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       })
       .catch((error) => console.log(error));
   };
