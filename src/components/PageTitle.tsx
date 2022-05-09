3e16c6b3ed7c5eec8e7b16bff8aa1ab8d16417df
spig0126 <spig0126@gmail.com>
2022-05-09 10:53:59 +0900

    🩹 Fix: navigation overlap prob

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,27 +1,34 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { FC } from "react";
 import { IPageTitle } from "../interfaces";
 
 const PageTitle: FC<IPageTitle> = ({ title, subtitle }) => {
   return (
-    <Flex
-      flexDirection="column"
-      position="relative"
-      top="-200"
-      zIndex="-1"
-      bgColor="blue"
-      p="15rem 6rem 5rem"
-      align="center"
-      w="100%"
-      mb="1rem"
-    >
-      <Heading as="h1" size="2xl">
-        {title}
-      </Heading>
-      <Heading as="h2" size="sm" mt="0.5rem">
-        {subtitle}
-      </Heading>
-    </Flex>
+    <>
+      <Box
+        bgColor="blue"
+        w="100%"
+        position="absolute"
+        h="10rem"
+        zIndex="-1"
+        top="0"
+      />
+      <Flex
+        flexDirection="column"
+        bgColor="blue"
+        p="5rem 6rem 3rem"
+        align="center"
+        w="100%"
+        mb="1rem"
+      >
+        <Heading as="h1" size="2xl">
+          {title}
+        </Heading>
+        <Heading as="h2" size="sm" mt="0.5rem">
+          {subtitle}
+        </Heading>
+      </Flex>
+    </>
   );
 };
 
