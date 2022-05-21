2c720f374f920bd37bb0ee498a8ba59ce9ca2670
spig0126 <spig0126@gmail.com>
2022-05-22 01:11:37 +0900

    ✨ Feat: add pageTitle layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,13 @@
 import { Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import PageTitle from "../src/components/PageTitle";
 
 const About: NextPage = () => {
-  return <Flex>About Page</Flex>;
+  return (
+    <Flex flexDir="column" w="100%">
+      <PageTitle title="About" subtitle="" />
+    </Flex>
+  );
 };
 
 export default About;
