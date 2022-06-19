a654045f1ada6d2aa25da16df1c04eff78af2a3e
Yongyeon Kim <yeonlotus@ewhain.net>
2022-06-20 01:32:41 +0900

    Remove unused codes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-import styled, { css } from "styled-components";
-import { Text, Heading, Flex, Button, Divider } from "@chakra-ui/react";
-import ThirdPartyLogin from "../src/components/LoginSignup/ThirdPartyLogin";
+import { Text, Heading, Flex, Button } from "@chakra-ui/react";
 import LocalLogin from "../src/components/LoginSignup/LocalLogin";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
@@ -72,38 +70,3 @@ const Login: NextPage = () => {
 };
 
 export default Login;
-
-const AnimatedFlex = styled.button<LoginProps>`
-  display: flex;
-  flex-direction: column;
-  width: 40%;
-  border-radius: 0.3rem;
-  align-items: center;
-  justify-content: center;
-  padding: 5rem 3rem;
-  background-color: pink;
-  margin: 0 auto;
-
-  animation: ${
-      (props) => (props.slideDir === "left" ? "slide-left" : "slide-right")
-      // if (props.slideDir === "left") {
-      //   return "slide-left";
-      // } else {
-      //   return "slide-right";
-      // }
-    }
-    infinite;
-
-  /* ${(props) =>
-    props.slideDir === "left"
-      ? css`
-          animation: slide-left 5s 1;
-        `
-      : css`
-          animation: slide-right;
-        `}; */
-
-  /* &:active {
-    animation: slide-right 1s 1;
-  } */
-`;
