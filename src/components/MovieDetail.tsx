fbb3eb02f5c78a177b50128e90a54f5d33811c42
Yongyeon Kim <yeonlotus@ewhain.net>
2022-07-03 20:39:45 +0900

    Update style of MovieDetail component

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -36,26 +36,35 @@ const MovieDetail = ({ detailData, creditData }: IDetailProps) => {
         <Stack>
           <Flex alignItems="center">
             <Heading mr={5}>{detailData?.title}</Heading>
-            <WatchButton movieId={movieId} />
           </Flex>
-          <Text>
-            <Text as="b">평점 </Text>
+          <Text display="flex" alignItems="center" fontWeight="light">
+            <Text fontWeight="normal" fontSize="lg">
+              평점 &nbsp;
+            </Text>
             {detailData?.vote_average}
           </Text>
-          <Text>
-            <Text as="b">개요</Text>{" "}
+          <Text display="flex" alignItems="center" fontWeight="light">
+            <Text fontWeight="normal" fontSize="lg">
+              개요 &nbsp;
+            </Text>
             {detailData?.genres && `${detailData.genres[0].name} | `}
             {detailData?.runtime}분 | {detailData?.release_date} 개봉
           </Text>
-          <Text>
-            <Text as="b">출연</Text> {creditData && creditData[0].name}
+          <Text display="flex" alignItems="center" fontWeight="light">
+            <Text fontWeight="normal" fontSize="lg">
+              출연 &nbsp;
+            </Text>
+            {creditData && creditData[0].name}
           </Text>
           <Text>{detailData?.overview}</Text>
         </Stack>
+
+        {/* 버튼 */}
         <HStack spacing={5}>
           <Button color="white" onClick={clickInfo} w="8rem" bg="brightBlue">
-            관련 정보
+            상세정보
           </Button>
+          <WatchButton movieId={movieId} />
           <GoodBadButton
             type="good"
             movieId={movieId}
