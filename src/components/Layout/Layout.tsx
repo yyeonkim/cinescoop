a2826edd4d5c79ddcd36ab07c8341103f3d2dad1
Yongyeon Kim <yeonlotus@ewhain.net>
2022-07-09 11:16:31 +0900

    ✨ Add Mobile Navigation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useMediaQuery } from "@chakra-ui/react";
+
 import Navigation from "./Navigation/Navigation";
 import Footer from "./Footer";
+import MobileNavigation from "./Navigation/MobileNavigation";
 
 interface layoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: layoutProps) => {
+  const [isSmallerThan640] = useMediaQuery("(max-width: 640px)");
+
   return (
     <>
       <Navigation search={true} />
+      {isSmallerThan640 && <MobileNavigation />}
       {children}
       <Footer />
     </>
--- src/components/Layout/Navigation/MobileNavigation.tsx
+++ src/components/Layout/Navigation/MobileNavigation.tsx
@@ -1,5 +1,9 @@
-import { Flex, Spacer, Image, Center, useMediaQuery } from "@chakra-ui/react";
+import { Flex, HStack, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { HiHome } from "react-icons/hi";
+import { FaUserFriends } from "react-icons/fa";
+import { MdMovie } from "react-icons/md";
+import { IoPersonCircle } from "react-icons/io5";
 
 import logo from "../../../public/logo.png";
 import Profile from "./Profile";
@@ -18,21 +22,43 @@ export interface IsSearch {
   search: boolean;
 }
 
-interface navigationProps {
-  search: boolean;
-}
-export default function MobileNavigation({ search }: navigationProps) {
-  const [isSmallerThan640] = useMediaQuery("(max-width: 640px)");
-
+export default function MobileNavigation() {
   return (
-    isSmallerThan640 && (
-      <Center px={5} zIndex={1} bg="brightBlue">
-        <Flex
-          justifyContent="space-between"
-          alignItems="center"
-          w="100%"
-        ></Flex>
-      </Center>
-    )
+    <HStack
+      w="100%"
+      bg="#1B1447"
+      position="fixed"
+      zIndex={10}
+      bottom={0}
+      p={5}
+      boxShadow="0 -3px 6px rgba(0, 0, 0, 0.16)"
+      justifyContent="center"
+      spacing={10}
+    >
+      <Link href="/">
+        <Flex direction="column" alignItems="center">
+          <HiHome size="1.5rem" />
+          <Text fontSize="sm">홈</Text>
+        </Flex>
+      </Link>
+      <Link href="/moviebuddy">
+        <Flex direction="column" alignItems="center">
+          <FaUserFriends size="1.5rem" />
+          <Text fontSize="sm">무비버디</Text>
+        </Flex>
+      </Link>
+      <Link href="/nowplaying">
+        <Flex direction="column" alignItems="center">
+          <MdMovie size="1.5rem" />
+          <Text fontSize="sm">상영중</Text>
+        </Flex>
+      </Link>
+      <Link href="/mypage">
+        <Flex direction="column" alignItems="center">
+          <IoPersonCircle size="1.5rem" />
+          <Text fontSize="sm">마이페이지</Text>
+        </Flex>
+      </Link>
+    </HStack>
   );
 }
